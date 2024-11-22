cee10e979f929123b0a9ee667a2002099c3a2dd4
Jurin Laiyakosit <jurin37@hotmail.co.th>
2024-11-22 10:22:57 +0700

    change user schema add date

--- models/user.model.js
+++ models/user.model.js
@@ -1,40 +1,45 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
-var id_implement = require("mongoose-sequence")(mongoose)
+var id_implement = require("mongoose-sequence")(mongoose);
 
-const userSchema = new Schema({
-  userId: {
-    type: Number,
-    unique: true,
+const userSchema = new Schema(
+  {
+    userId: {
+      type: Number,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    surname: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    sex: {
+      type: String,
+      required: true,
+    },
   },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  surname: {
-    type: String,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  sex: {
-    type: String,
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
-userSchema.plugin(id_implement,{id:"userId",inc_field:"userId"});
+userSchema.plugin(id_implement, { id: "userId", inc_field: "userId" });
 
 const User = mongoose.model("users", userSchema);
 
